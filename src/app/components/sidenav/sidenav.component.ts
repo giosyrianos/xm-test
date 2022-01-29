e1d876b944564adf28652a4f41b679f00ce68c87
gio <giosyrianos@gmail.com>
2022-01-29 21:42:58 +0200

    feat: setup sidenav

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+	@Output() menuToggled = new EventEmitter<boolean>();
+
+	showMenu = false;
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	toggleMenu() {
+		this.showMenu = !this.showMenu;
+		this.menuToggled.emit(this.showMenu)
+	}
+
+}
--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -1,14 +1,17 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
-  exports: [
+	exports: [
 		MatButtonModule,
+		MatIconModule,
+		MatExpansionModule,
 		MatSidenavModule
-  ]
+	]
 })
 
 export class AngularMaterialModule { }
