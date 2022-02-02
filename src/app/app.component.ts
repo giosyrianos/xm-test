6b4a8be031709c020f1e692f91fb938ff17a88ef
gio <giosyrianos@gmail.com>
2022-02-02 17:55:58 +0200

    feat: react to window resizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component,OnInit, OnDestroy} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	destroyed = new Subject<void>();
+	currentScreenSize: string;
 // this should be provided by a service
 
 	title = `Lorem ipsum dolor? Excepteur sint occaecat!`;
@@ -13,6 +19,42 @@ export class AppComponent {
 	magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat.`
 	isMenuVisible = false;
+
+	displayNameMap = new Map([
+		[Breakpoints.XSmall, 'Small'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
+
+
+	constructor(public breakpointObserver: BreakpointObserver) {
+		this.setScreenSize()
+	 }
+
+	ngOnInit(): void {
+
+	}
+
+	setScreenSize() {
+		this.breakpointObserver.observe([
+			Breakpoints.XSmall,
+			Breakpoints.Small,
+			Breakpoints.Medium,
+			Breakpoints.Large,
+			Breakpoints.XLarge,
+		])
+		.pipe(takeUntil(this.destroyed))
+		.subscribe(result => {
+			console.log(result)
+			for (const query of Object.keys(result.breakpoints)) {
+				if (result.breakpoints[query]) {
+					this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+				}
+			}
+		});
+	}
 
 	hideContent(e: any) {
 		this.isMenuVisible = e;
